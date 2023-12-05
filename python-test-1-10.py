355114767fb9784baa3feb784724b5327e40c8c5
Kangratia <kangratia@gmail.com>
2023-12-06 00:27:50 +0900

    test complete

--- python-test-1-10.py
+++ python-test-1-10.py
@@ -14,3 +14,40 @@ print ('신씨가 소리질렀다. "도둑이야"')
 #004 print 기초
 #화면에 C:\Windows를 출력하세요.
 print ("C:\Windows")
+
+#005 print 탭과 줄바꿈
+#다음 코드를 실행해보고 \t와 \n의 역할을 설명해보세요.
+
+print("안녕하세요.\n만나서\t\t반갑습니다.")
+
+#\n은 줄 바꿈을 의미하고, \t는 탭을 의미합니다.
+
+#006 print 여러 데이터 출력
+# print 함수에 두 개의 단어를 입력한 예제입니다. 아래 코드의 출력 결과를 예상해봅시다.
+
+print ("오늘은", "일요일")
+
+#예측: 오늘은 일요일 이라고 출력 될 예정.
+
+#007 print 기초
+# print() 함수를 사용하여 다음과 같이 출력하세요.
+# naver;kakao;sk;samsung
+
+print ("naver;kakao;sk;samsung")
+print ("naver", "kakao", "sk", "samsung",sep=";") #sep 인자로 ";"를 넣어줌으로써 구분자를 변경할 수 있습니다.
+
+#008 print 기초
+# print() 함수를 사용하여 다음과 같이 출력하세요.
+# naver/kakao/sk/samsung
+print ("naver/kakao/sk/samssung")
+print ("naver", "kakao", "sk", "samsung",sep="/") #sep 인자로 "/"를 넣어줌으로써 구분자를 변경할 수 있습니다.
+
+#009 print 기초
+#다음 코드를 수정하여 줄바꿈이 없이 출력하세요. (힌트: end='') print 함수는 두 번 사용합니다. 세미콜론 (;)은 한줄에 여러 개의 명령을 작성하기 위해 사용합니다.
+#print("first");print("second")
+print("first", end=' '); print("second") #end 인자로 ""를 넣어줌으로써 줄바꿈을 없앨 수 있습니다.
+print("first");print("second") #비교
+
+#010 연산 결과 출력
+# 5/3의 결과를 화면에 출력하세요.
+print(5/3)
